d5936bf515370dd39b980f2fe3f7fd2ed59ae014
manos <manosangelis@gmail.com>
2021-12-08 20:46:14 +0200

    My first commit

---
+++ main.py
@@ -0,0 +1,121 @@
+import argparse
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+import torch.backends.cudnn as cudnn
+import os
+import wandb
+from utils.data_processing.get_dataset import get_train_data, get_validation_data
+from utils.models import *
+from utils.utilities.utils import progress_bar
+
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+best_acc = 0  # best test accuracy
+start_epoch = 0  # start from epoch 0 or last checkpoint epoch
+
+def train(epoch,net,trainloader,criterion,optimizer):
+    print('\nEpoch: %d' % epoch)
+    net.train()
+    train_loss = 0
+    correct = 0
+    total = 0
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        loss = criterion(outputs, targets)
+        loss.backward()
+        optimizer.step()
+
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        total += targets.size(0)
+        correct += predicted.eq(targets).sum().item()
+
+        progress_bar(batch_idx, len(trainloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                     % (train_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+    acc=correct/total
+    wandb.log({"train_loss":1-acc},step=epoch)
+
+def test(epoch,net,testloader,criterion):
+    global best_acc
+    net.eval()
+    test_loss = 0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for batch_idx, (inputs, targets) in enumerate(testloader):
+            inputs, targets = inputs.to(device), targets.to(device)
+            outputs = net(inputs)
+            loss = criterion(outputs, targets)
+
+            test_loss += loss.item()
+            _, predicted = outputs.max(1)
+            total += targets.size(0)
+            correct += predicted.eq(targets).sum().item()
+
+            progress_bar(batch_idx, len(testloader), 'Loss: %.3f | Acc: %.3f%% (%d/%d)'
+                         % (test_loss/(batch_idx+1), 100.*correct/total, correct, total))
+
+
+    # Save checkpoint.
+    acc = 100.*correct/total
+    if acc > best_acc:
+        print('Saving..')
+        state = {
+            'net': net.state_dict(),
+            'acc': acc,
+            'epoch': epoch,
+        }
+        if not os.path.isdir('checkpoint'):
+            os.mkdir('checkpoint')
+        torch.save(state, './checkpoint/ckpt.pth')
+        best_acc = acc
+
+    acc=acc/100
+    wandb.log({"classification_loss":1-acc},step=epoch)
+
+def construct_and_train(args: dict):
+    global start_epoch
+    trainset1,trainset2 = get_train_data()
+    testset = get_validation_data()
+    trainloader = torch.utils.data.DataLoader(trainset1,batch_size=128, shuffle= True,num_workers=2)
+    testloader = torch.utils.data.DataLoader(testset,batch_size=100, shuffle= False,num_workers=1)
+
+    net = ResNet18()
+    net = net.to(device)
+    if device == 'cuda':
+        net = torch.nn.DataParallel(net)
+        cudnn.benchmark = True
+    wandb.init(project='optas_project_21', name='CIFAR10_RESNET18_2SUBSETS', config=args)
+    wandb.watch(net)
+
+    if args['resume']:
+        # Load checkpoint.
+        print('==> Resuming from checkpoint..')
+        assert os.path.isdir('checkpoint'), 'Error: no checkpoint directory found!'
+        checkpoint = torch.load('./checkpoint/ckpt.pth')
+        net.load_state_dict(checkpoint['net'])
+        best_acc = checkpoint['acc']
+        start_epoch = checkpoint['epoch']
+
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.SGD(net.parameters(), lr=args.lr,
+                      momentum=0.9, weight_decay=5e-4)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=args['epochs'])
+
+    for epoch in range(start_epoch, start_epoch + args['epochs']):
+        train(epoch,net,trainloader,criterion,optimizer)
+        test(epoch,net,testloader,criterion)
+        scheduler.step()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="PyTorch experiments")
+    parser.add_argument('--lr', default=0.1, type=float, help='learning rate')
+    parser.add_argument('--resume', '-r', action='store_true', help='resume from checkpoint')
+    parser.add_argument("--epochs",default=200,type=int, choices=range(0,401), help="number of epochs")
+    args = vars(parser.parse_args())
+    construct_and_train(args)
---
+++ utils/data_processing/get_dataset.py
@@ -0,0 +1,37 @@
+import torch
+from torchvision import datasets, transforms
+
+CIFAR_TRAIN_DATASET_SIZE=50000
+
+class CIFAR10Transform:
+    def train_transform():
+        return transforms.Compose([
+               transforms.RandomCrop(32, padding=4),
+               transforms.RandomHorizontalFlip(),
+               transforms.ToTensor(),
+               transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),])
+
+    def test_transform():
+        return transforms.Compose([
+               transforms.ToTensor(),
+               transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),])
+
+def get_train_data():
+    dataset= datasets.CIFAR10(root='../data', train=True, download=True, transform=CIFAR10Transform.train_transform())
+    dataset1=torch.utils.data.Subset(dataset,range(CIFAR_TRAIN_DATASET_SIZE/2))
+    dataset2=torch.utils.data.Subset(dataset,range(CIFAR_TRAIN_DATASET_SIZE/2,CIFAR_TRAIN_DATASET_SIZE))
+
+    dataset.targets = np.array(dataset.targets)
+    print("Splitting training dataset into 2 subsets")
+    a=dataset.targets[np.array(range(CIFAR_TRAIN_DATASET_SIZE/2))]
+    unique, counts = np.unique(a, return_counts=True)
+    print("The first  one has target counts ", counts)
+    b=dataset.targets[np.array(range(CIFAR_TRAIN_DATASET_SIZE/2,CIFAR_TRAIN_DATASET_SIZE))]
+    unique, counts = np.unique(b, return_counts=True)
+    print("The second one has target counts ", counts)
+
+    return dataset1,dataset2
+
+def get_validation_data():
+    dataset= datasets.CIFAR10(root='../data', train=False, download=True, transform=CIFAR10Transform.test_transform())
+    return dataset
